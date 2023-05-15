aa64afb92ed6871421f886ff8b75aa07a888e245
Jairo <jairomora61@gmail.com>
2023-05-15 16:51:52 +0200

    Creado menu Home y rutas de redireccionamiento protegidas

---
+++ src/views/Repuestos.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app
+    id="inspire"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <SideBar />
+    <Navbar />
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="9">
+          <v-row>
+            <v-col cols="12" sm="12">
+              <Contact />
+            </v-col>
+            <v-col cols="12" sm="12">
+              <TableCrudRepuestos />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <Doctor />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import SideBar from "@/components/SideBar.vue";
+import Navbar from "../components/Navbar.vue";
+import Contact from "../components/Contact.vue";
+import Doctor from "../components/Doctor.vue";
+import TableCrudRepuestos from "../components/TableCRUDRepuestos.vue";
+
+export default {
+  data: () => ({}),
+
+  components: {
+    SideBar,
+    Navbar,
+    Contact,
+    Doctor,
+    TableCrudRepuestos, 
+  },
+};
+</script>
